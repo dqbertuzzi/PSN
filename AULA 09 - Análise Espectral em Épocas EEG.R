20fd985a1627fb02d8bbb92b5caad198d5273532
Daniel Bertuzzi <30902283+dqbertuzzi@users.noreply.github.com>
2017-11-20 12:16:21 -0200

    Rename AULA 9 - Análise Espectral em Épocas EEG.R to AULA 09 - Análise Espectral em Épocas EEG.R

--- AULA 09 - Análise Espectral em Épocas EEG.R
+++ AULA 09 - Análise Espectral em Épocas EEG.R
@@ -17,8 +17,8 @@ plot(espectro$freq*hz, log(espectro$spec), type='l') # Espectro bruto, sinal sem
 filtro <- signal::butter(5, c(1,30)/(hz/2), 'pass')
 fsinal <- signal::filter(filtro, sinais[,29])
 
-# Definir tamanho da época (depende da situação que pode gerar artefatos)
-# Criar épocas
+# Definir tamanho da Ã©poca (depende da situaÃ§Ã£o que pode gerar artefatos)
+# Criar Ã©pocas
 tamEpoca <- 10 #tam. da epoca em segundos
 
 Nepocas <- floor(length(fsinal)/(hz*tamEpoca))
@@ -27,33 +27,33 @@ media <- 0
 
 # Controle de qualidade rigoroso
 amplitude <- array(0, Nepocas)
-cutoff <- 60 # Para descartar épocas com alta amplitude
+cutoff <- 60 # Para descartar Ã©pocas com alta amplitude
 NepocasBoas <- 0
 
-#Percorre todas as épocas
+#Percorre todas as Ã©pocas
 for (epoca in 1:Nepocas){
         inicio <- hz*tamEpoca*(epoca-1)+1
         fim <- hz*tamEpoca*epoca
         janela <- inicio:fim
-        amplitude[epoca] <- max(fsinal[janela]) - min(fsinal[janela]) #Variação do sinal naquela época
+        amplitude[epoca] <- max(fsinal[janela]) - min(fsinal[janela]) #VariaÃ§Ã£o do sinal naquela Ã©poca
         
         #Periodograma do sinal nesta EPOCA
         espectro <- spectrum(fsinal[janela], plot=F)
         freq <- espectro$freq
         
-        #Tira média do espectro
-        #Só das épocas que passaram no controle de qualidade
+        #Tira mÃ©dia do espectro
+        #SÃ³ das Ã©pocas que passaram no controle de qualidade
         if (amplitude[epoca] < cutoff){
         media <- media + espectro$spec
         NepocasBoas <- NepocasBoas + 1
         
         }
 }
-media <- media/NepocasBoas #Computa a média só das épocas boas
-#Plot do Espectro Médio
+media <- media/NepocasBoas #Computa a mÃ©dia sÃ³ das Ã©pocas boas
+#Plot do Espectro MÃ©dio
 plot(freq, media, type='l',
      xlab = 'Freq(Hz)',
-     ylab = 'Espectro Médio')
+     ylab = 'Espectro MÃ©dio')
 
 #Histograma das amplitudes
 hist(amplitude[which(amplitude < 1000)]) # Retirando os valores muito altos
